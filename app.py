e6a10c881f5c03f85e91cebdd15308fe743c29df
mlum23 <mlum23@my.bcit.ca>
2020-12-27 18:02:22 -0800

    Added layout html page; Submit button works

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, redirect, request
 import statsmodels.api as sm
 import pandas as pd
 import matplotlib.pyplot as plt
+import pandas_datareader as pdr
 
 
 app = Flask(__name__)
@@ -11,14 +12,21 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         stock_name = request.form['stock-name']
-        return redirect(url_for('prediction', stock=stock_name))
+        try:
+            stock_data = pdr.get_data_yahoo(stock_name)
+        except (TypeError, KeyError):
+            error = "Stock name does not exist"
+            return render_template("index.html", error=error)
+        else:
+            return render_template('prediction.html', stock_name=stock_name)
     else:
         return render_template('index.html')
 
 
 @app.route('/prediction/<stock>')
 def prediction(stock):
-    return 'The stock name is ' + str(stock)
+    stock_name = 'The stock name is ' + str(stock)
+    return render_template('prediction.html', stock_name=stock_name)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas_datareader as pdr
 
 pd.set_option("display.max_columns", 100)
-stock_open = pdr.get_data_yahoo('MSFT')['Open']
+stock_open = pdr.get_data_yahoo('asdasd')['Open']
 
 # Last 30 days
 stock_open_last_30 = stock_open[len(stock_open) - 30:len(stock_open)]
